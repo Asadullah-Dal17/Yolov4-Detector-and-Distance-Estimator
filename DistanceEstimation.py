973ec26d6d4ccdb48176a34384315f34321f394e
asadullah-dal17 <asadullah.dal17@gmail.com>
2021-07-04 19:26:33 +0500

    updating distance estimation

--- DistanceEstimation.py
+++ DistanceEstimation.py
@@ -1,10 +1,62 @@
 import cv2 as cv 
 import numpy as np
 
+# Distance constants 
+KNOWN_DISTANCE = 45 #INCHES
+PERSON_WIDTH = 16 #INCHES
+MOBILE_WIDTH = 3.0 #INCHES
+
+# Object detector constant 
+CONFIDENCE_THRESHOLD = 0.4
+NMS_THRESHOLD = 0.3
+
+# colors for object detected
+COLORS = [(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
+
+GREEN = (0, 255, 0)
+RED = (0, 0, 255)
+BLACK = (0, 0, 0)
+YELLOW = (0, 255, 255)
+WHITE = (255, 255, 255)
+CYAN = (255, 255, 0)
+MAGENTA = (255, 0, 242)
+GOLDEN = (32, 218, 165)
+LIGHT_BLUE = (255, 9, 2)
+PURPLE = (128, 0, 128)
+CHOCOLATE = (30, 105, 210)
+PINK = (147, 20, 255)
+ORANGE = (0, 69, 255)
+
+FONTS = cv.FONT_HERSHEY_COMPLEX
+class_names = []
+with open("classes.txt", "r") as f:
+    class_names = [cname.strip() for cname in f.readlines()]
+#  setttng up opencv net
+yoloNet = cv.dnn.readNet('yolov4-tiny.weights', 'yolov4-tiny.cfg')
+
+yoloNet.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
+yoloNet.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA_FP16)
+
+model = cv.dnn_DetectionModel(yoloNet)
+model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
+
+# focal length finder function 
+def FocalLength(measured_distance, real_width, width_in_rf_image):
+
+    focal_length = (width_in_rf_image * measured_distance) / real_width
+    return focal_length
+
+# distance estimation function
+def Distance_finder(Focal_Length, real_object_width, object_width_in_frame):
+
+    distance = (real_object_width * Focal_Length)/object_width_in_frame
+    return distance
+
 cap = cv.VideoCapture(1)
+
 while True:
     ret, frame = cap.read()
-    cv.imshow('frame')
+    cv.imshow('frame',frame)
     key = cv.waitKey(1)
     if key ==ord('q'):
         break
