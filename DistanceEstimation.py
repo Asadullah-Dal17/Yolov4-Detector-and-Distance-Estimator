dc5b8bfd21d522b543d5ea58041d285ad386ea39
asadullah-dal17 <asadullah.dal17@gmail.com>
2021-07-05 15:36:31 +0500

    updating code

--- DistanceEstimation.py
+++ DistanceEstimation.py
@@ -13,20 +13,6 @@ NMS_THRESHOLD = 0.3
 # colors for object detected
 COLORS = [(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
 
-GREEN = (0, 255, 0)
-RED = (0, 0, 255)
-BLACK = (0, 0, 0)
-YELLOW = (0, 255, 255)
-WHITE = (255, 255, 255)
-CYAN = (255, 255, 0)
-MAGENTA = (255, 0, 242)
-GOLDEN = (32, 218, 165)
-LIGHT_BLUE = (255, 9, 2)
-PURPLE = (128, 0, 128)
-CHOCOLATE = (30, 105, 210)
-PINK = (147, 20, 255)
-ORANGE = (0, 69, 255)
-
 FONTS = cv.FONT_HERSHEY_COMPLEX
 class_names = []
 with open("classes.txt", "r") as f:
@@ -40,6 +26,7 @@ yoloNet.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA_FP16)
 model = cv.dnn_DetectionModel(yoloNet)
 model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
 
+
 # focal length finder function 
 def FocalLength(measured_distance, real_width, width_in_rf_image):
 
@@ -51,21 +38,17 @@ def Distance_finder(Focal_Length, real_object_width, object_width_in_frame):
 
     distance = (real_object_width * Focal_Length)/object_width_in_frame
     return distance
-def object_detector(image):
-    # getting class names, confindance, bounding boxes for the model. 
-    classes, scores, boxes = model.detect(image, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
-    DataList =[]
-    # getting the chassid , confidence and bounding box for each indivdual object
-    for (classid, score, box) in zip (classes, scores, boxes):
-        color = COLORS[int(classid)% len(COLORS)]
-        label = "%s : %f" %(class_names[classid])
 
 
-cap = cv.VideoCapture(1)
+
+
+cap = cv.VideoCapture(3)
 
 while True:
     ret, frame = cap.read()
+    object_detector(frame)
     cv.imshow('frame',frame)
+    
     key = cv.waitKey(1)
     if key ==ord('q'):
         break
